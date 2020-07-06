d231532b6fb46b23f3f3376528adf0cb474318de
al315233 <44438333+al315233@users.noreply.github.com>
2020-07-06 13:32:03 +0200

    detalles
    detalles y bugs

--- TFG/Assets/Scripts/FirstTextInstructions.cs
+++ TFG/Assets/Scripts/FirstTextInstructions.cs
@@ -2,12 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
 
 public class FirstTextInstructions : MonoBehaviour
 {
 
+    private GameObject player;
+
     public GameObject instructions;
-    public GameObject player;
+
+    bool activo = false;
 
     //Puzzle 2
     public GameObject manager;
@@ -16,7 +21,10 @@ public class FirstTextInstructions : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        if(FirstPersonController.Instance != null)
+        {
+            player = FirstPersonController.Instance.gameObject;
+        }
         //Todos los puzzles
         instructions.SetActive(true);
 
@@ -36,9 +44,17 @@ public class FirstTextInstructions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
 
+        if (Input.GetKeyDown(KeyCode.Space) && !activo)
+        {
+            activo = true;
+            switch (SceneManager.GetActiveScene().name)
+            {
+                case GlobalData.MUSEUM_SCENE_KEY:
+                    Camera.main.gameObject.SetActive(false);
+                    break;
+            }
+            
             //Todos los puzzles
             instructions.SetActive(false);
 
---
+++ TFG/Assets/Scripts/VolumeSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().volume = GlobalData.GameVolume;
+    }
+}
